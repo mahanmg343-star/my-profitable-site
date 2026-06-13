57147c70f689b2b6b60a5c1e1a4aaef8a39dd240
mahanmg343-star <mahanmg343@gmail.com>
2026-06-13 13:40:25 +0330

    homework

---
+++ src/Debug.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Debug() {
+  return (
+
+
+<img src="https://www.rahatyad.ir/images/rahatyad_logo.svg" alt="" />
+
+)
+}
+
+export default Debug
---
+++ src/Sec2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function sec2() {
+  return (
+    <p>jdddddf</p>
+  )
+}
+
+export default sec2
